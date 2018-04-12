2e2c98334240b960110d2935d9ae52d3a55b9249
cryosx <cyruswu.email@gmail.com>
2018-04-12 09:18:23 -1000

    Finished

--- challenge_6.js
+++ challenge_6.js
@@ -6,8 +6,28 @@
  */
 exports.SumOfASquare = SumOfASquare;
 
-function SumOfASquare(naturalNumbers){
+function SumOfASquare(naturalNumbers) {
   this.naturalNumbers = naturalNumbers;
+
+  return {
+    sumOfSquares: sumOfSquares.bind(null, naturalNumbers),
+    squareOfTheSums: squareOfTheSums.bind(null, naturalNumbers)
+  };
+
+  function sumOfSquares(num) {
+    let result = 0;
+    for (let i = 1; i <= num; i++) {
+      result += Math.pow(i, 2);
+    }
+    return result;
+  }
+  function squareOfTheSums(num) {
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+      sum += i;
+    }
+    return Math.pow(sum, 2);
+  }
 }
 
 // do work here
